9c4457c05de9eda8daf67cca969a02fc519113de
Antony Anil <anil.antony@settlin.in>
2022-06-22 09:55:35 +0530

    Render added add user v1

--- server/services/render.js
+++ server/services/render.js
@@ -1,3 +1,18 @@
-exports.homeRoutes = (req,res) => {res.render('index')}
-exports.add_user = (req,res) => {res.render('add_user')}
-exports.update_user = (req,res) => {res.render('update_user')}
+const axios = require('axios')
+
+exports.homeRoutes = (req,res) => {
+    //make a get request to axios users
+    axios.get('http://localhost:3000/api/users')
+        .then((response)=>{
+            res.render('index',{user:response.data})
+        })
+        .catch(err=>{
+            res.status(400).send({message:'error retreiving data'})
+        })
+}
+exports.add_user = (req,res) => {
+    res.render('add_user')
+}
+exports.update_user = (req,res) => {
+    res.render('update_user')
+}
